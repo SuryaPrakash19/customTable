863b3f217e1e335d482c153ad1f782325ee9152e
Surya Prakash S <suryas190299@gmail.com>
2024-12-01 18:10:01 +0530

    basic table

--- src/custom-table/custom-table.component.ts
+++ src/custom-table/custom-table.component.ts
@@ -1,10 +1,11 @@
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-custom-table',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe, JsonPipe, MatTableModule],
   templateUrl: './custom-table.component.html',
   styleUrl: './custom-table.component.css',
 })
